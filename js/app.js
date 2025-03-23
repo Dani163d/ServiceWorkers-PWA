ca8e1fe7e74fa0f2cdd503bac166a9ebc327d007
Dani163d <Danielasilvacastillo163@gmail.com>
2025-03-23 07:30:51 -0400

    detrectar el soporte de service workers

---
+++ js/app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+    .then( registrado => console.log('se instalo correctamente', registrado))
+    .catch(error => console.log('fallo la instalacion', error));
+} else {
+    console.log ('servicio no soportado')
+}
