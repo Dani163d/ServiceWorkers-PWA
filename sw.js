bcb780fe91b7ad2cb7b2f840c36f5da1a9076ded
Dani163d <Danielasilvacastillo163@gmail.com>
2025-03-24 20:30:43 -0400

    agregando soporte offline

--- sw.js
+++ sw.js
@@ -1,18 +1,61 @@
-// cuando se instala el service worker
+// Nombre del cache
+const nombreCache = 'apv-cache';
+const assets = [
+    '/',
+    '/index.html',
+    '/error.html',
+    '/js/app.js',
+    '/css/bootstrap.css',
+    '/css/styles.css'
+];
+
+// console.log(self);
+
+// Instalar el service Worker, no se puede utilizar window, por lo tanto se utiliza self
 self.addEventListener('install', e => {
-    console.log('Instalado el service worker');
+    console.log('Instalado el Service Worker');
 
-    console.log(e);
+    e.waitUntil(
+        // Buen lugar para cachear - 
+        caches.open(nombreCache)
+            .then(cache => { // Esta función es asincrona...
+                console.log('cacheando...');
+                cache.addAll(assets);
+            })
+    );
+
+    // Revisar en App (Chrome) en Firefox en Almacenamiento
 });
 
-// activar el service worker
+// Activar el service worker...
 self.addEventListener('activate', e => {
-    console.log('service worker activado');
+    console.log('Service Worker Activado');
 
-    console.log(e);
+    // Actualizar la PWA //
+    e.waitUntil(
+        caches.keys()
+            .then(keys => {
+                console.log(keys); 
+
+                return Promise.all(keys
+                        .filter(key => key !== nombreCache)
+                        .map(key => caches.delete(key)) // borrar los demas
+                    )
+            })
+    )
 });
 
-// evento fetch para descargar archivos estaticos
+// Fetch events para el CSS, HTML, imagenes JS, y hasta llamados a fetch..
 self.addEventListener('fetch', e => {
-    console.log('fetch...', e);
-})
+    console.log('Fetch.. ', e);
+
+    e.respondWith(
+        caches.match(e.request)
+            .then(respuestaCache => {
+                return respuestaCache || fetch(e.request);
+            })
+            .catch(() => caches.match('/error.html'))
+    );
+});
+
+// Al tener este fetch, un nombre de app, y una página de inicio, ya podremos agregar nuestra PWA a la página de inicio...
